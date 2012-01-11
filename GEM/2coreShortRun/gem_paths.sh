195061510c856eba46b6aba2abd1bd7d61025a3a
Srinath Vadlamani <srinath@txcorp.com>
2012-01-11 12:49:56 -0700

    Test case for 2 cores and few time steps.
    This will be the right test case for developing vsh5 output.

---
+++ GEM/2coreShortRun/gem_paths.sh
@@ -0,0 +1,11 @@
+######################################################################
+#
+# $Id: gem_paths.sh.in 114 2011-01-17 17:44:28Z srinath $
+#
+# Purpose: run a simple example of GEM code from CU
+#
+# Copyright Tech-X, 2007
+#
+######################################################################
+
+export topbuilddir=/Users/srinath/work/txProjects/valtools_proj/gem/autotooled/build1
